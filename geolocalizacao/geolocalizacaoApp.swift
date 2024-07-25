6ce2ab5e940d32c007b9ef4fd5fdc432c0b6a331
Daniel Lopes <daniel-lps@hotmail.com.br>
2024-07-25 19:57:36 -0300

    Initial Commit

---
+++ geolocalizacao/geolocalizacaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  geolocalizacaoApp.swift
+//  geolocalizacao
+//
+//  Created by Daniel Lopes da Silva on 25/07/24.
+//
+
+import SwiftUI
+
+@main
+struct geolocalizacaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
